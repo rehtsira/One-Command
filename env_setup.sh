729639a634b7cebb40fe2da0631b37df602d09c1
rehtsira <64707170+rehtsira@users.noreply.github.com>
2021-09-20 17:51:41 -0400

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Purpose: Install the required packages to install Virtual Manager and Ansible. After that is complete, a connection must be established between the two servers using ssh-keygen. 
+
 #Install apt packages
 apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager ssh python3.8 python3-pip software-properties-common -y
 
@@ -16,7 +18,7 @@ ufw allow ssh
 adduser 'aer' libvirtd
 adduser 'aer' kvm
 
-#Once virtual machine is created with the Ubuntu ISO, set up the SSH keys manually between the host and remote server.
+#Once the virtual machine is created with an Ubuntu ISO, set up the SSH keys manually between the host and remote server with these commands. Change the IP Address with your remote server's IP and also the home location.
 #ssh-keygen -t rsa 
 #Cd ~/.ssh
 #cat id_rsa.pub >> authorized_keys
